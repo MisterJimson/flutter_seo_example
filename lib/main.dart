ec60ad2f6807f0266e8290d2e96b3d40ddf58be1
Jason Rai <me@jasonrai.ca>
2022-05-14 14:20:58 -0400

    feat: add basic pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,118 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+const String title = 'Jason\'s Movie Reviews';
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+void main() => runApp(App());
 
-  // This widget is the root of your application.
+class App extends StatelessWidget {
+  App({Key? key}) : super(key: key);
+
+  final _router = GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const HomePage(),
+      ),
+      GoRoute(
+        path: '/review/:slug',
+        builder: (context, state) {
+          final review =
+              reviews.firstWhere((r) => r.slug == state.params['slug']);
+          return ReviewPage(review: review);
+        },
+      ),
+    ],
+  );
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return MaterialApp.router(
+      routeInformationParser: _router.routeInformationParser,
+      routerDelegate: _router.routerDelegate,
+      title: title,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      appBar: AppBar(title: const Text(title)),
+      body: ListView.builder(
+          itemCount: reviews.length,
+          itemBuilder: (_, index) {
+            var review = reviews[index];
+            return ListTile(
+              title: Text(review.movieTitle),
+              onTap: () => context.push(
+                '/review/${review.slug}',
+              ),
+            );
+          }),
+    );
+  }
+}
+
+class ReviewPage extends StatelessWidget {
+  final MovieReview review;
+
+  const ReviewPage({
+    Key? key,
+    required this.review,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text(title)),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              review.movieTitle,
+              style: Theme.of(context).textTheme.headline3,
+            ),
+            const SizedBox(height: 20),
+            Text(
+              review.review,
+              style: Theme.of(context).textTheme.bodyMedium,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+class MovieReview {
+  final String slug;
+  final String movieTitle;
+  final String review;
+  final int rating;
+
+  const MovieReview(
+    this.slug,
+    this.movieTitle,
+    this.review,
+    this.rating,
+  );
+}
+
+const reviews = [
+  MovieReview('avengers-endgame', 'Avengers: Endgame',
+      'Great movie! Crazy start to finish!', 9),
+  MovieReview(
+      'eternal-Sunshine-of-the-spotless-mind',
+      'Eternal Sunshine of the Spotless Mind',
+      'Fantasic story, mind bending and amazing performances!',
+      9),
+  MovieReview('the-shawshank-redemption', 'The Shawshank Redemption',
+      'Great movie!', 9),
+  MovieReview('the-godfather', 'The Godfather', 'Great movie!', 9),
+];
